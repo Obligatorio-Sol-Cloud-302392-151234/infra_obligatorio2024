a1cf1add61bfe8323a8438e85652e6481e99a0f3
gs151234 <159207952+gs151234@users.noreply.github.com>
2024-11-21 22:38:43 -0300

    Se corrige nombres de vpc y subredes. se crear archivo de varibles.tf

--- network.tf
+++ network.tf
@@ -1,29 +1,34 @@
-resource "aws_vpc" "practico-terraform-vpc" {
-  cidr_block           = "10.0.0.0/16"
+resource "aws_vpc" "prod_app_vpc" {
+  cidr_block           = var.network_cidr
   enable_dns_support   = true
   enable_dns_hostnames = true
 
   tags = {
-    Name = "test-terraform-vpc"
+    Name = "vpc-prod-app"
   }
 }
 
-resource "aws_subnet" "practico-terraform-subnet" {
-  vpc_id                  = aws_vpc.practico-terraform-vpc.id
-  availability_zone       = "us-east-1a"
-  cidr_block              = "10.0.0.0/24"
+resource "aws_subnet" "public_subnet" {
+  count                   = length(var.public)
+  vpc_id                  = aws_vpc.prod_app_vpc.id
+  cidr_block              = element(var.public, count.index)
+  availability_zone       = element(var.azone, count.index)
   map_public_ip_on_launch = "true"
+
   tags = {
-    Name = "test-terraform-subnet"
+    Name = "Public Subnet ${count.index + 1}"
   }
 }
-resource "aws_subnet" "practico-terraform-subnet2" {
-  vpc_id                  = aws_vpc.practico-terraform-vpc.id
-  availability_zone       = "us-east-1b"
-  cidr_block              = "10.0.1.0/24"
+
+resource "aws_subnet" "prevate_subnet" {
+  count                   = length(var.private)
+  vpc_id                  = aws_vpc.prod_app_vpc.id
+  cidr_block              = element(var.private, count.index)
+  availability_zone       = element(var.azone, count.index)
   map_public_ip_on_launch = "true"
+
   tags = {
-    Name = "test-terraform-subnet2"
+    Name = "Private Subnet ${count.index + 1}"
   }
 }
 
---
+++ variables.tf
@@ -0,0 +1,21 @@
+variable "network_cidr" {
+  type        = string
+  description = "Variable para CIDR"
+}
+
+variable "azone" {
+  type        = list(string)
+  default     = ["us-east-1a", "us-east-1b"]
+  description = "Variable para la AZ"
+}
+
+variable "public" {
+  type        = list(string)
+  description = "Variable de Subnet publica"
+}
+
+variable "private" {
+  type        = list(string)
+  description = "Variable de Subnet privada"
+}
+
