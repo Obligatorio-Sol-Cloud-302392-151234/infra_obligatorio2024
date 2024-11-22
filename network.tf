859224d50ee4046fa81bb09d369d0655c266bac5
gs151234 <159207952+gs151234@users.noreply.github.com>
2024-11-21 23:22:16 -0300

    Se agrega ig,nat y rt. se asocian rt y subnets

--- network.tf
+++ network.tf
@@ -4,7 +4,7 @@ resource "aws_vpc" "prod_app_vpc" {
   enable_dns_hostnames = true
 
   tags = {
-    Name = "vpc-prod-app"
+    Name = "prod_app_vpc"
   }
 }
 
@@ -16,11 +16,11 @@ resource "aws_subnet" "public_subnet" {
   map_public_ip_on_launch = "true"
 
   tags = {
-    Name = "Public Subnet ${count.index + 1}"
+    Name = "public_subnet ${count.index + 1}"
   }
 }
 
-resource "aws_subnet" "prevate_subnet" {
+resource "aws_subnet" "private_subnet" {
   count                   = length(var.private)
   vpc_id                  = aws_vpc.prod_app_vpc.id
   cidr_block              = element(var.private, count.index)
@@ -28,8 +28,69 @@ resource "aws_subnet" "prevate_subnet" {
   map_public_ip_on_launch = "true"
 
   tags = {
-    Name = "Private Subnet ${count.index + 1}"
+    Name = "private_subnet ${count.index + 1}"
   }
+}
+
+resource "aws_internet_gateway" "internet_gw" {
+  vpc_id = aws_vpc.prod_app_vpc.id
+
+  tags = {
+    Name = "internet_gw"
+  }
+}
+
+resource "aws_eip" "public_ip" {
+  vpc = true
+}
+
+resource "aws_nat_gateway" "nat_gw" {
+  allocation_id = aws_eip.public_ip.id
+  subnet_id     = var.public[0]
+
+  depends_on = [aws_internet_gateway.gw]
+
+  tags = {
+    Name = "nat_gw"
+  }
+}
+
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.prod_app_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.internet_gw.id
+  }
+
+  tags = {
+    Name = "public_rt"
+  }
+}
+
+resource "aws_route_table" "private_rt" {
+  vpc_id = aws_vpc.prod_app_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.nat_gw.id
+  }
+
+  tags = {
+    Name = "private_rt"
+  }
+}
+
+resource "aws_route_table_association" "public_subnet_as" {
+  count          = length(var.public)
+  subnet_id      = element(aws_subnet.public_subnets[*].id, count.index)
+  route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_route_table_association" "private_subnet_as" {
+  count          = length(var.private)
+  subnet_id      = element(aws_subnet.private_subnets[*].id, count.index)
+  route_table_id = aws_route_table.private_rt.id
 }
 
 
