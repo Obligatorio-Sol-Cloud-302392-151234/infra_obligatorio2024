178f851d3cab21a988cc228b83930fb751c070ec
gs151234 <159207952+gs151234@users.noreply.github.com>
2024-11-24 20:17:47 -0300

    cambios en errores de tipeo en variables en mail de deply

--- deploy/main.tf
+++ deploy/main.tf
@@ -1,7 +1,7 @@
 resource "null_resource" "run_local_command" {
   provisioner "local-exec" {
     command = <<EOT
-      aws eks update-kubeconfig --region ${var.region} --name ${var.app_eks_cluster} --alias ${app_eks_cluster.name} --profile ${profile}
+      aws eks update-kubeconfig --region ${var.region} --name ${var.app_eks_cluster} --alias ${var.app_eks_cluster} --profile ${var.profile}
       bash deploy/deploy_k8s.sh
     EOT
   }
